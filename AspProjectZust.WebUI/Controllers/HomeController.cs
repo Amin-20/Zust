eb33b354739d8d37d68d3bc38f882f1454980101
Ataki_Pw11 <Ataki_Pw11@STHQ0123-11>
2023-12-20 16:39:37 +0400

    Add project files.

---
+++ AspProjectZust.WebUI/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspProjectZust.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Birthday()
+        {
+            return View();
+        }
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult Favorite()
+        {
+            return View();
+        }
+
+
+        public IActionResult Friends()
+        {
+            return View();
+        }
+
+        public IActionResult Groups()
+        {
+            return View();
+        }
+
+        public IActionResult HelpAndSupport()
+        {
+            return View();
+        }
+
+        public IActionResult LiveChat()
+        {
+            return View();
+        }
+
+        public IActionResult MarketPlace()
+        {
+            return View();
+        }
+
+        public IActionResult Messages()
+        {
+            return View();
+        }
+
+
+        public IActionResult MyProfile()
+        {
+            return View();
+        }
+
+        public IActionResult NewsFeed()
+        {
+            return View();
+        }
+
+        public IActionResult Notifications()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Setting()
+        {
+            return View();
+        }
+
+        public IActionResult Video()
+        {
+            return View();
+        }
+
+        public IActionResult Weather()
+        {
+            return View();
+        }
+    }
+}
+
